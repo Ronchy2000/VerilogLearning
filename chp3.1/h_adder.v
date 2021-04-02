1ded3e5db71a63067e821a38d363974c530c06bc
Ronchy <rongqi1949@gmail.com>
2021-04-02 10:48:48 +0800

    Chapter3 Finished

--- chp3.1/h_adder.v
+++
@@ -1,6 +0,0 @@
-module h_adder (A,B,SO,CO);
-	input A,B;
-	output SO,CO;
-	assign CO = A & B;
-	assign SO = A ^ B;
-endmodule
--- chp3.20/MULT4B_parameter2.v
+++
@@ -1,6 +0,0 @@
-module MULT4B_parameter2(RP,AP,BP);
-	output[15:0] RP;
-	input[7:0] AP,BP;
-		MULT4B_parameter_base #(.S(8))
-		U1 (.R(RP), .A(AP),.B(BP));
-endmodule
