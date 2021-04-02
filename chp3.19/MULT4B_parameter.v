47a1bf4e419b16e74d198816ba4014c631b41d6e
Ronchy <rongqi1949@gmail.com>
2021-04-02 10:14:42 +0800

    Chp3 finished

---
+++ chp3.19/MULT4B_parameter.v
@@ -0,0 +1,19 @@
+module MULT4B_parameter #(parameter S=4)(R,A,B);
+//parameter S=4;
+output[2*S:1] R;
+input[S:1] A,B;
+reg[2*S:1] AT,R;
+reg[S:1] BT,ST;
+ always @(A,B)
+	begin
+		R=0;
+		AT={{S{1'b0}},A};
+		BT=B;
+		for(ST=S;ST>0;ST=ST-1)
+		begin
+			if(BT[1]) R=R+AT;
+			AT=AT<<1;
+			BT=BT>>1;
+		end
+	end
+endmodule
